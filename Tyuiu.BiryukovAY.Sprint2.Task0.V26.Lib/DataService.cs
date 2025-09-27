f6309d9461c8a797c5df00f3088fca8802c9d145
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-27 19:40:32 +0700

    Task0

--- Tyuiu.BiryukovAY.Sprint2.Task0.V26.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint2.Task0.V26.Lib/DataService.cs
@@ -5,21 +5,16 @@ namespace Tyuiu.BiryukovAY.Sprint2.Task0.V26.Lib
     {
         public bool[] GetCompareOperations(int x, int y)
         {
-            int difference = x - y; 
+            bool[] result = new bool[6];
 
-            bool[] results = new bool[5];
+            result[0] = x == y; 
+            result[1] = x != x;   
+            result[2] = x < y;  
+            result[3] = x > y;    
+            result[4] = x <= x;    
+            result[5] = x >= x;     
 
-            results[0] = x == y + 69; 
-
-            results[1] = x != y + 70; 
-
-            results[2] = x < y + 70; 
-
-            results[3] = x > y + 69; 
-
-            results[4] = x <= y + 70; 
-
-            return results;
+            return result;
         }
     }
 }
