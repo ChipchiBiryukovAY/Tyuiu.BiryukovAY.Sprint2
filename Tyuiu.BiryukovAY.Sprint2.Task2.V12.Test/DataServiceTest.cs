466eea4f65cf2897fda3b855addad66836e81916
ChipchiBiryukovAY <chipichix@yandex.ru>
2025-09-27 18:57:47 +0500

    Add files via upload

---
+++ Tyuiu.BiryukovAY.Sprint2.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tyuiu.BiryukovAY.Sprint2.Task2.V12.Lib;
+namespace Tyuiu.BiryukovAY.Sprint2.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.IsTrue(ds.CheckDotInShadedArea(4, 5));
+        }
+    }
+}
