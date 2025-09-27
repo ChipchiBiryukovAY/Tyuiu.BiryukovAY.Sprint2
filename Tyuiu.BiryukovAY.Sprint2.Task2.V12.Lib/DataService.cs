b9727843449bb6a533ebc35750ef580fc7ef22e3
ChipchiBiryukovAY <chipichix@yandex.ru>
2025-09-27 18:52:51 +0500

    Add files via upload

---
+++ Tyuiu.BiryukovAY.Sprint2.Task2.V12.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.BiryukovAY.Sprint2.Task2.V12.Lib
+{
+    public class DataService : ISprint2Task2V12
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            if (x >= 3 && x <= 5 && y >= 3 && y <= 7)
+                return true;
+
+            if (x >= 6 && x <= 12 && y >= 5 && y <= 8)
+                return true;
+
+            if (x >= 9 && x <= 12 && y >= 3 && y <= 4)
+                return true;
+
+            if (x == 13 && y >= 6 && y <= 8)
+                return true;
+
+            if (x >= 6 && x <= 7 && y >= 9 && y <= 11)
+                return true;
+
+            if (x >= 3 && x <= 5 && y == 11)
+                return true;
+
+            if (x == 7 && y == 12)
+                return true;
+
+            if (x >= 11 && x <= 12 && y >= 9 && y <= 12)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Tyuiu.BiryukovAY.Sprint2.Task2.V12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tyuiu.BiryukovAY.Sprint2.Task2.V12.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Write("Введите X: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите Y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        bool result = ds.CheckDotInShadedArea(x, y);
+        Console.WriteLine($"Результат: {result}");
+    }
+}
